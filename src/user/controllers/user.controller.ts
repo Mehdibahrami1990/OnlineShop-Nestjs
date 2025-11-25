87baa6343b6fe07c7153806bb64c46bc776ea13a
mehdi <tbmgroup20@gmail.com>
2025-11-25 23:21:16 +0330

    feat: create security user module /user.service&user.controller

--- src/user/controllers/user.controller.ts
+++ src/user/controllers/user.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { UserService } from '../services/user.service';
+import { UserQueryDto } from '../dtos/user-query.dto';
+import { UserDto } from '../dtos/user.dto';
+// import { ApiTags } from '@nestjs/swagger';
 
+// @ApiTags('User')
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  findAll(@Query() queryParams: UserQueryDto) {
+    return this.userService.findAll(queryParams);
+  }
+  @Post()
+  create(@Body() body: UserDto) {
+    return this.userService.create(body);
+  }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(id);
+  }
+  @Put(':id')
+  update(@Param('id') id: string, @Body() body: UserDto) {
+    return this.userService.update(id, body);
+  }
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.userService.delete(id);
+  }
+}
